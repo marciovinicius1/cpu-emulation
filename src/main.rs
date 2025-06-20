e8a5815838b7743beb621cedb3f6f38b839f4d15
Márcio Vinícius <101148317+marciovinicius1@users.noreply.github.com>
2025-06-20 01:36:32 -0300

    Expand CPU functionality to support opcode reading and execution
    Increased register count to 16 and implemented a basic event loop to decode and execute opcodes. Added logic for `add_xy` with overflow handling and memory initialization for instruction execution. Included opcode test cases to validate functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ struct CPU {
     registers: [u8; 16],
     position_in_memory: usize,
     memory: [u8; 0x1000],
-    // stack: [u16; 16],
-    // stack_pointer: usize
+    stack: [u16; 16],
+    stack_pointer: usize
 }
 
 impl CPU {
@@ -25,8 +25,13 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+            // let kk = (opcode & 0x00FF) as u8
+            
             match (c, x, y, d) {
                 (0, 0, 0, 0) => return,
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _ , _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
@@ -47,6 +52,29 @@ impl CPU {
             self.registers[0xF] = 0;
         }
     }
+
+    fn call(&mut self, addr:u16) {
+        let sp = self.stack_pointer; //current index of stack
+        let stack = &mut self.stack; //stack 
+        
+        if sp > stack.len() {
+            panic!("Stack overflow")
+        }
+        
+        stack[sp] = self.position_in_memory as u16; //current stack now is current memory position
+        self.stack_pointer += 1; //stack pointer increase
+        self.position_in_memory = addr as usize; //read code from a new place
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1; //decrement stack
+        let call_addr = self.stack[self.stack_pointer]; // call addr
+        self.position_in_memory = call_addr as usize;
+    }
 }
 
 fn main() {
@@ -54,39 +82,35 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
-        // stack: [0; 16],
-        // stack_pointer: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
+    
 
     let mem = &mut cpu.memory;
     // setting 2 bytes opcode/line
-    // 0x8014 -> add the value in 0 (5) the value in register 1 (10)
-    mem[0] = 0x80; mem[1] = 0x14;
-    // 0x8024 -> add the value in 0 (15) the value in register 2 (10)
-    mem[2] = 0x80; mem[3] = 0x24;
-    // 0x8034 -> add the value in 0 (25) the value in register 3 (10)
-    mem[4] = 0x80; mem[5] = 0x34;
+    // 0x2100 -> call the function at 0x001 addr memory
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    // 0x2100 -> call the function at 0x001 addr memory
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    // 0x0000 -> set return addr with 0
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
 
+    //fn add_twice()
+    // 0x8014 -> add register y to register x
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    // 0x8014 -> add register y to register x
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    // return 
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
+    
     cpu.run();
-    assert_eq!(cpu.registers[0], 35);
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
-
-    // //create add_twice 3 opcodes
-    // let add_twice: [u8; 6] = [
-    //     //0x8014 -> add the value in 0 (5) the value in register 1 (10)
-    //     0x80, 0x14,
-    //     //0x8014 -> add the value in 0 (15) the value in register 1 (10)
-    //     0x80, 0x14,
-    //     //return
-    //     0x00, 0xEE,
-    // ];
-    // //load add_twice function in memory
-    // mem[0x100..0x106].copy_from_slice(&add_twice);
+    
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
 
 
